3439af1ae3543e019083f15f9159c444bdce8bee
Travis Zusa Zuve Saputra <traviszusa.2022@student.uny.ac.id>
2022-10-12 22:21:33 +0700

    12 Oktober 2022

---
+++ src/Semester_1/Minggu_07/Algoritma/Exercise04.java
@@ -0,0 +1,34 @@
+package Semester_1.Minggu_07.Algoritma;
+
+import java.util.Scanner;
+
+public class Exercise04 {
+    public static void main(String[] args) {
+        Scanner user_input = new Scanner(System.in);
+
+        String terpanjang = "";
+        boolean seri = false;
+
+        System.out.print("How many people? ");
+        int orang = user_input.nextInt();
+
+        for (int i = 1; i <= orang; i++) {
+            System.out.print("name #" + i + "? ");
+            String sekarang = user_input.next();
+
+            if(sekarang.length() == terpanjang.length()) {
+                seri = true;
+            } 
+            if (sekarang.length() > terpanjang.length()) {
+                seri = false;
+                terpanjang = sekarang;
+            }
+        }
+        String all = terpanjang.substring(0, 1).toUpperCase() + terpanjang.substring(1).toLowerCase();
+        System.out.println(all + "'s name is longest");
+
+        if (seri) {
+            System.out.println("There was a tie!");
+        }
+    }
+}
