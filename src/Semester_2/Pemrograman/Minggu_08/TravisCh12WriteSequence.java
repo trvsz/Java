8cb2f69a5e28e593e54420ae3247cb521fcb8888
Travis Zusa Zuve Saputra <traviszusa.2022@student.uny.ac.id>
2023-03-16 14:14:30 +0700

    Upload File

---
+++ src/Semester_2/Pemrograman/Minggu_08/TravisCh12WriteSequence.java
@@ -0,0 +1,48 @@
+package Semester_2.Pemrograman.Minggu_08;
+
+// Travis Zusa Zuve Saputra
+// 22537141013
+
+public class TravisCh12WriteSequence {
+    public static void main(String[] args) {
+        writeSequence(1);
+        System.out.println();
+        writeSequence(2);
+        System.out.println();
+        writeSequence(3);
+        System.out.println();
+        writeSequence(4);
+        System.out.println();
+        writeSequence(5);
+        System.out.println();
+        writeSequence(6);
+        System.out.println();
+        writeSequence(7);
+        System.out.println();
+        writeSequence(8);
+    }
+    public static void writeSequence(int n) {
+        if (n < 1) {
+            throw new IllegalArgumentException();
+        } else if (n == 1) {
+            System.out.print("1 ");
+        } else if (n == 2) {
+            System.out.print("1 1 ");
+        } else {
+            System.out.print((n + 1) / 2 + " ");
+            writeSequence(n - 2);
+            System.out.print((n + 1) / 2 + " ");
+        }
+    }
+}
+
+/*
+1 
+1 1 
+2 1 2 
+2 1 1 2 
+3 2 1 2 3 
+3 2 1 1 2 3 
+4 3 2 1 2 3 4 
+4 3 2 1 1 2 3 4 
+*/
